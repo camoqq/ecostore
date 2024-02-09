0fb625663642122c0d39cde6483464502fbbc9db
camokkkk <93404887+camokkkk@users.noreply.github.com>
2024-02-09 13:38:33 -0600

    change

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
-const express = require('express');
-const db = require('./config/connection');
+const express = require("express");
+const db = require("./config/connection");
 
-const PORT = 3001;
+const PORT = 5000;
 const app = express();
+app.get("/", (req, res) => {
+  console.log("first");
+  res.send("hi");
+});
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-db.once('open', () => {
+db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`running on port ${PORT}!`);
   });
